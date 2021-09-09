bd780373bdbe76a8c4418affce8277feb0559d38
Wanderson Aldo <wanderson.aldo@gmail.com>
2021-09-09 12:55:20 -0300

    API Noticias

---
+++ controllers/noticia.controller.js
@@ -0,0 +1,37 @@
+const db = require("../models");
+const Tutorial = db.noticias;
+
+// Create and Save a new Noticia
+exports.create = (req, res) => {
+  
+};
+
+// Retrieve all Noticia from the db.
+exports.findAll = (req, res) => {
+  
+};
+
+// Find a single Noticia with an id
+exports.findOne = (req, res) => {
+  
+};
+
+// Update a Noticia by the id in the request
+exports.update = (req, res) => {
+  
+};
+
+// Delete a Noticia with the specified id in the request
+exports.delete = (req, res) => {
+  
+};
+
+// Delete all Noticia from the db
+exports.deleteAll = (req, res) => {
+  
+};
+
+// Find all published Noticias
+exports.findAllPublished = (req, res) => {
+  
+};
